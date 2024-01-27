53680c4f180b3c12434fa27f8d36e58fa59d7b95
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2024-01-27 10:41:24 +0530

    Delete projects/FD Calculator directory

--- projects/FD Calculator/script.js
+++
@@ -1,9 +0,0 @@
-function calculateamt(){
-    let principle = parseFloat(document.getElementById('principle').value);
-    let interest = parseFloat(document.getElementById('interest').value);
-    let time = parseFloat(document.getElementById('time').value);
-    let calc = (principle * interest * time )/100;
-    let ans = (principle +calc);
-    document.getElementById('result').innerHTML = `Maturity Amount : ${ans.toFixed(2)}`
-}
-document.getElementById('btn').addEventListener('click', calculateamt);
